12eb792601dd9cb7b68329464e3c2276d7b41c13
ryuichi7 <brettrheenan@gmail.com>
2015-11-30 19:29:07 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,10 @@
-# Enter your procedural solution here!
+def collect_multiples(int)
+  index = 1.upto(int - 1).to_a
+  index.select do |number|
+    number % 3 == 0 || number % 5 == 0
+  end
+end
+
+def sum_multiples(int)
+  collect_multiples(int).inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,19 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(int)
+    raise ArgumentError if int > 1000
+    @int = int
+  end
+
+  def collect_multiples
+    index = 1.upto(@int - 1).to_a
+    index.select do |number|
+      number % 3 == 0 || number % 5 == 0
+    end
+  end
+
+  def sum_multiples
+    collect_multiples.inject(:+)
+  end
+end
+
